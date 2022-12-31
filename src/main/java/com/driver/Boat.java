19fbdcc566408d6e2b96e1803869e0b54ab42ffa
Sourabh <createownpath111@gmail.com>
2022-12-31 13:11:45 +0530

    Vehicle assignment code

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -3,6 +3,10 @@ package com.driver;
 public class Boat implements WaterVehicle{
     String name;
     int capacity;
+
+    public Boat(){
+
+    }
     @Override
     public String getVehicleName() {
         return name;
